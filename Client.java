32122ff0db4c6ef77b1086590fc5bded79731905
Gabriel Dias <gaah@pop-os.localdomain>
2023-05-27 18:50:37 -0300

    Desenvolvimento final de PvE

--- Client.java
+++ Client.java
@@ -10,43 +10,42 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
     
-    try{
-    Socket socketClient = new Socket(IP_SERVIDOR, PORTA);
+        try{
+            Socket socketClient = new Socket(IP_SERVIDOR, PORTA);
 
-    //Saida de dadoss do cliente
-    PrintWriter saidasDados = new PrintWriter(socketClient.getOutputStream(), true);
-    
-    //Entrada de dados vinda do servidor
-    BufferedReader entradaDados = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            //Saida de dadoss do cliente
+            PrintWriter saidasDados = new PrintWriter(socketClient.getOutputStream(), true);
+            
+            //Entrada de dados vinda do servidor
+            BufferedReader entradaDados = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            
+            while (true){
+                // Requisição do servidor: 
+                String entrada = entradaDados.readLine();
+                if(entrada == null){
+                    break;
+                }
+                System.out.println(entrada);
 
-    BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-    
-    while (true){
-        // Requisição do servidor: 
-        String entrada = entradaDados.readLine();
-        if(entrada == null){
-            break;
+                /* Se entrada de dados vindo do servidor terminar com "Escolha"
+                habilitamos o buffer de digitação de saidas de dados do cliente*/
+
+                if(entrada.endsWith("Escolha:")){
+                    String saida = console.readLine();
+                    saidasDados.println(saida);
+                }
+            }
+
+            saidasDados.close();
+            entradaDados.close();
+            console.close();
+            socketClient.close();
+
         }
-        System.out.println(entrada);
-
-        /* Se entrada de dados vindo do servidor começar com "Sua vez de jogar"
-        habilitamos o buffer de digitação de saidas de dados do cliente*/
-
-        if(entrada.startsWith("Sua vez de jogar")){
-            String saida = console.readLine();
-            saidasDados.println(saida);
+        catch (Exception e) {
+            System.out.println("Erro no cliente ao se conectado ao servidor: " + e.getMessage());
+            return;
         }
-    }
-
-    saidasDados.close();
-    entradaDados.close();
-    console.close();
-    socketClient.close();
-
-    }catch (Exception e) {
-        System.out.println("Erro no cliente ao se conectado ao servidor: " + e.getMessage());
-        return;
-    }
-
     }
 }
--- Jogo.java
+++ Jogo.java
@@ -6,9 +6,12 @@ import java.util.Random;
 
 public class Jogo implements Runnable {
 
-    private Socket[] socketClient;
+    private Socket[] socketsClient;
     private PrintWriter[] saidasDados;
     private BufferedReader[] leiturasDados;
+    private Socket socketClient;
+    private PrintWriter saidaDados;
+    private BufferedReader leituraDados;
     
 
     /*Dentro do nosso jogo temos:
@@ -20,7 +23,19 @@ public class Jogo implements Runnable {
     private static int[][] COMBI_VENCEDORAS = {{0, 2}, {1, 0}, {2, 1}};
     private static int MAXIMO_JOGADERES= 2;
 
-    // Aqui se concentra o nucleu principal da lógica do jogo
+    //Construtores com os parametros coletados da classe servidor:
+    public Jogo(Socket[] socketsClient, PrintWriter[] saidasDados, BufferedReader[] leiturasDados) {
+        this.socketsClient = socketsClient;
+        this.saidasDados = saidasDados;
+        this.leiturasDados = leiturasDados;
+    }
+    public Jogo(Socket socketClient, PrintWriter saidaDados, BufferedReader leituraDados) {
+        this.socketClient = socketClient;
+        this.saidaDados = saidaDados;
+        this.leituraDados = leituraDados;
+    }
+
+    // Aqui se concentra o nucleo principal da lógica do jogo
     
     private static int getResultado(int escolhasClient1 , int escolhasClient2) {
         //Se as escolhas forem igual resulta em empate
@@ -49,72 +64,17 @@ public class Jogo implements Runnable {
         }
         return true;
     }
-
-    // Embaixo pegamos as escolhas dos clientes: 
     
-    //Construtor com os parametos coletados da classe servidor:
-    public Jogo(Socket[] socketClient, PrintWriter[] saidasDados, BufferedReader[] leiturasDados) {
-        this.socketClient = socketClient;
-        this.saidasDados = saidasDados;
-        this.leiturasDados = leiturasDados;
-    }
-
     @Override
     public void run() {
-        int escolhaClient = -1;
-
-        for (int i = 0; i < MAXIMO_JOGADERES; i++) {
-            saidasDados[i].println("***Bem vindo ao jogo Pedra, Papel e Teoura***\n");
-            while(true) {
-                saidasDados[i].println("Você gostaria de jogar contra a máquina, ou contra um jogador?\n0 -> máquina\n1 -> jogador");
-                saidasDados[i].println("Escolha:");
-                try {
-                    escolhaClient = Integer.parseInt(leiturasDados[i].readLine());
-                } catch (IOException e) {
-                    System.out.println("Jogador " + (i + 1) + " digitou uma escolha inválida.");
-                }
-                boolean flag = getValidade(new int[] {0,1}, escolhaClient);
-                if (flag) {
-                    break;
-                }
-            }
-        }
-   
-        if (escolhaClient == 0) {
-            while(true) {
-                saidasDados[0].println("Escolhas sua opção: \n 0 -> pedra \n 1 -> papel \n 2 -> tesoura ");
-                saidasDados[0].println("Sua vez de jogar. Digite:");
-                try {
-                    escolhaClient = Integer.parseInt(leiturasDados[0].readLine());
-                } catch (IOException e) {
-                    System.out.println("Jogador 0 digitou uma escolha inválida.");
-                }
-
-                Random rand = new Random();
-                int num = rand.nextInt(2);
-
-                int resultado = getResultado(escolhaClient, num);
-
-                if (resultado == -1) {
-                    saidasDados[0].println("Empate!");
-                }
-                else if (resultado == 0) {
-                    saidasDados[0].println("Você venceu!");
-                }
-                else {
-                    saidasDados[0].println("A máquina venceu!");
-                }
-            }
-        }
-
-        else if (escolhaClient == 1) {
+        if (socketClient == null && saidaDados == null && leituraDados == null) {
             while (true) {
                 int[] escolhasClient = new int[MAXIMO_JOGADERES];
     
                 // Recebe as escolhas dos jogadores
                 for (int i = 0; i < MAXIMO_JOGADERES; i++) {
                     saidasDados[i].println("Escolhas sua opção: \n 0 -> pedra \n 1 -> papel \n 2 -> tesoura ");
-                    saidasDados[i].println("Sua vez de jogar. Digite:");
+                    saidasDados[i].println("Sua vez de jogar. Escolha:");
                     try {
                         escolhasClient[i] = Integer.parseInt(leiturasDados[i].readLine());
                     } catch (IOException e) {
@@ -128,9 +88,8 @@ public class Jogo implements Runnable {
                 for (int i = 0; i < MAXIMO_JOGADERES; i++) {
                     flag = getValidade(new int[] {0, 2}, escolhasClient[i]);
                 }
-    
                 // Se alguma escolha for inválida, pula para a próxima rodada
-                if (flag) {
+                if (!flag) {
                     continue;
                 }
     
@@ -138,18 +97,51 @@ public class Jogo implements Runnable {
                 int resultado = getResultado(escolhasClient[0], escolhasClient[1]);
                 String saidaResultado = "";
                 if (resultado == -1) {
-                  
                     saidaResultado = "Empate!";
-                    } else {
-                    
+                } else {
                     saidaResultado = "Jogador " + (resultado + 1) + " venceu!";
-                    }
+                }
     
-            // Envia a String do resultado do jogo para cada jogador
-            for (int i = 0; i < MAXIMO_JOGADERES; i++) {
-                saidasDados[i].println(saidaResultado);
+                // Envia a String do resultado do jogo para cada jogador
+                for (int i = 0; i < MAXIMO_JOGADERES; i++) {
+                    saidasDados[i].println(saidaResultado);
+                }
             }
-          }
+        }
+
+        else if (socketsClient == null && saidasDados == null && leiturasDados == null) {
+            while(true) {
+                saidaDados.println("Escolhas sua opção: \n 0 -> pedra \n 1 -> papel \n 2 -> tesoura ");
+                saidaDados.println("Sua vez de jogar. Escolha:");
+                int escolhaClient = -1;
+                try {
+                    escolhaClient = Integer.parseInt(leituraDados.readLine());
+                } catch (IOException e) {
+                    System.out.println("Jogador 0 digitou uma escolha inválida.");
+                }
+
+                // Verifica se as escolhas dos jogadores são válidas
+                boolean flag = getValidade(new int[] {0, 2}, escolhaClient);
+                // Se alguma escolha for inválida, pula para a próxima rodada
+                if (!flag) {
+                    continue;
+                }
+
+                Random rand = new Random();
+                int num = rand.nextInt(3);
+
+                int resultado = getResultado(escolhaClient, num);
+
+                if (resultado == -1) {
+                    saidaDados.println("Empate!");
+                }
+                else if (resultado == 0) {
+                    saidaDados.println("Você venceu!");
+                }
+                else {
+                    saidaDados.println("A máquina venceu!");
+                }
+            }
+        }
         }
     }
-}
